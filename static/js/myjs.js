ec851dab7d97d19e71d0e12d50c8f88790159bea
amnuhu91 <amnuhu91@yahoo.com>
2021-08-17 10:21:30 +0100

    working on myjs

--- static/js/myjs.js
+++ static/js/myjs.js
@@ -62,80 +62,15 @@ $(document).ready(function(){
     // console.log(res2.data);
   }));
   
-   
+
+});
+
+const sr = document.getElementById('search-id')
+sr.addEventListener('keyup', function(){
+  alert('keyup')
 });
 
 
 
 
 
-// const myform = document.getElementById('reg')
-// myform.addEventListener('submit',(event)=>{
-//   event.preventDefault();
-//   //console.log(myform);
-//   const csrfmiddlewaretoken =  document.querySelector('[name=csrfmiddlewaretoken]').value;
-//   const last_name = document.getElementById('post-l_name');
-//   const midle_name = document.getElementById('post-midle');
-//   const first_name = document.getElementById('post-f_name');
-//   //console.log(csrfmiddlewaretoken);
-//   const fd = new FormData();
-  
-//   fd.append('f_name',first_name.value)
-//   fd.append('midle',midle_name.value)
-//   fd.append('l_name',last_name.value)
-//   fd.append("csrfmiddlewaretoken",csrfmiddlewaretoken)
- 
-//   // for(let val of fd){
-//   //   console.log(val);
-//   // }
-//   axios.post("",fd)
-//   .then((response)=>{
-//     let data = response.data;
-//     console.log(data);
-//     // for(let i=0; i<data.length; i++){
-//     //   let per = data[i];
-//     //   console.log(per.id, per.f_name,);
-      
-//     // }
-//     for(let obj in data){
-//       console.log(`${obj}:  :${data[obj].f_name},${data[obj].midle}${data[obj].l_name}
-      
-//       `);
-//     }
-//     if(response.status==200){
-//       first_name.value=''
-//       midle_name.value=''
-//       last_name.value=''
-//       //myform.reset()
-//       first_name.focus()
-//       $('#view-per').click().hide(0);
-
-
-//     }
-//     else{
-//       console.log('something went wrong');
-//     }
-    
-
-//   })
-//   .catch((err)=>{
-//     console.log(err);
-//   });
-    
-  
-// });
-
-// const viewPer = document.getElementById('view-per');
-// viewPer.addEventListener('click', function(){
-//   const percontainer= document.getElementById('pers')
-  
-//   axios.get('/person')
-//       .then((response)=>{
-//           //console.log(response.data);
-//         percontainer.innerHTML=response.data;
-
-//       })
-//       .catch((err)=>{
-//         console.log(err);
-//       });
-// });
