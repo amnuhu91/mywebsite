1695a27d62fafcf087371bc1d9df9ae0f9312afd
amnuhu91 <amnuhu91@yahoo.com>
2022-06-17 13:41:46 +0100

    saved my twilio recovery code in this file

--- blog/send_mtn_sms.py
+++ blog/send_mtn_sms.py
@@ -1,24 +1,7 @@
-import requests as rq 
+acsid = 'ACca84f51b32c8edfde7ac10a898cc6320'
+token = 'c9e6f4e26edf9ab317317e623ee77ea1'
+
+phone = '+19785064523'
 
 
-def send_sms():
-	url = "https://api.mtn.com/v2/messages/sms/outbound"
-
-	payload = {
-	    "senderAddress": "08064790955",
-	    "receiverAddress": ["08162472682"],
-	    "message": "this is from me Abubakar",
-	    "clientCorrelator": "string12345"
-	}
-	headers = {
-	    "Content-Type": "application/json",
-	    #"Authorization": "",
-	    "x-api-key":"HhYDrgnil2aKGHgrDnrNjfEI7LZuztP6"
-	    #"secret_key":"S6gzEJX2z6otYGZL"
-	    
-	}
-
-	response = rq.request("POST", url, json=payload, headers=headers)
-
-	print(response.text)
-send_sms()
+recovery code = 'xcs4QprB5Q4nqPzLNlnNlR5D3An9uM9i3Aq2EOgn'
