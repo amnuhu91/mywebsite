b509af02308d95dfd7269ea1a748d80faa06ec35
Abubakar Muhammad Nuhu <42860986+amnuhu91@users.noreply.github.com>
2022-07-19 16:46:35 +0100

    Update send_sms_twilio.py
    updated twilio auth token

--- blog/send_sms_twilio.py
+++ blog/send_sms_twilio.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client 
  
 account_sid = 'AC77c58d059917dee5f2198c168f66d8b2' 
-auth_token = '57190a857a8dd5c311a6331cd19b1ef4'
+auth_token = 'ce6b20bb151b62e856031ea7bd3a2174'
  
 
 
